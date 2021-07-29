5c1b1c6106dd25144969865548f77edbc5a7cf7d
LeeJunHui00 <62248164+LeeJunHui00@users.noreply.github.com>
2021-07-29 14:14:53 +0900

    fin

--- Face_Detector.py
+++ Face_Detector.py
@@ -16,18 +16,11 @@ while True:
                                                   randrange(256), randrange(256)), 3)
 
     cv2.imshow('Face Detector', frame)
-    cv2.waitKey(1)
+    key = cv2.waitKey(1)
 
+    if key == 81 or key == 113:
+        break
 
-# face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+webcam.release()
 
-# # print(face_coordinates)
-# for (x, y, w, h) in face_coordinates:
-#     cv2.rectangle(img, (x, y), (x+w, y+h), (randrange(256),
-#                                             randrange(256), randrange(256)), 3)
-
-# cv2.imshow('Face Detector', img)
-# cv2.waitKey()
-
-
-# print("Code Completed")
+print("Code Completed")
